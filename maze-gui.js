0384a0d829ef0c3187cba268722b3e91ad1e703b
Robert Müller <robert.mueller@uni-siegen.de>
2021-04-07 13:07:56 +0200

    add hotkeys, update code style, improve canvas performance

--- maze-gui.js
+++ maze-gui.js
@@ -1,13 +1,10 @@
-function replaceAll(target, search, replacement)
-{
+function replaceAll(target, search, replacement) {
 	return target.replace(new RegExp(search, 'g'), replacement);
 };
 
-function createSandboxedColorFunction(code)
-{
+function createSandboxedColorFunction(code) {
 	var frame = document.getElementById('sandbox');
-	if(frame == null)
-	{
+	if(frame == null) {
 		var frame = document.createElement('iframe');
 		frame.src = "about:blank";
 		frame.style.display = "none";
@@ -22,12 +19,11 @@ function createSandboxedColorFunction(code)
 var settings = {
 	running : true,
 	speed : 100,
-	complete : function()
-	{
+	performStep : maze.performStep,
+	complete : function() {
 		$("*").addClass("wait");
 		// call is delayed to let the browser show the wait-cursor
-		setTimeout(function()
-		{
+		setTimeout(function() {
 			maze.completeGeneration();
 			$("*").removeClass("wait");
 		}, 100);
@@ -35,25 +31,20 @@ var settings = {
 	},
 	startX : 0,
 	startY : 0,
-	pickStartPosition : function()
-	{
+	pickStartPosition : function() {
 		alert("Click on the maze to select a new start position. Press H to hide/show the controls.");
 		$("#floatingBox").text("Click to select a start position");
 		$("#canvas").off().on({
-			mouseenter : function(e)
-			{
+			mouseenter : function(e) {
 				$("#floatingBox").show();
 			},
-			mouseleave : function(e)
-			{
+			mouseleave : function(e) {
 				$("#floatingBox").hide();
 			},
-			mousemove : function(e)
-			{
+			mousemove : function(e) {
 				$("#floatingBox").css({ left : e.pageX, top : e.pageY });
 			},
-			click : function(e)
-			{
+			click : function(e) {
 				settings.startX = e.pageX / $("#canvas").width();
 				settings.startY = e.pageY / $("#canvas").height();
 				gui.updateDisplay();
@@ -69,97 +60,69 @@ var settings = {
 	circleProbability : 0.0,
 	backgroundColor : "#000000",
 	colorFactor : 3600,
-	colorFunction : function(c, x, y)
-	{
+	colorFunction : function(c, x, y) {
 		return "hsl(" + (c * 360.0) + ", 100%, 50%)";
 	},
 	colorFunctionPreset : "rainbow",
-	changeColorFunction : function()
-	{
+	changeColorFunction : function() {
 		$("#colorFunctionPresets").empty();
-		Object.keys(colorFunctions).forEach(function(key, index)
-		{
-			$("#colorFunctionPresets").append("<option>" + key + "</option>");
-		});
-		$("#colorFunctionPresetLoad").off('click').on('click', function()
-		{
+		Object.keys(colorFunctions).forEach((key, index) => $("#colorFunctionPresets").append("<option>" + key + "</option>"));
+		$("#colorFunctionPresetLoad").off('click').on('click', () => {
 			var f = colorFunctions[$("#colorFunctionPresets").val()].toString();
 			var b = f.slice(f.indexOf("{") + 1, f.lastIndexOf("}"));
 			$("#colorFunction").val(replaceAll(b, "\n\t\t", "\n").trim());
 		});
-	
 		$("#dialog-wrapper, #dialog-colorFunction").fadeIn();
-		$(".cancel-button").off('click').on('click', function()
-		{
-			$("#dialog-wrapper, .dialog").fadeOut();
-		});
-		function checkSyntax()
-		{
-			try
-			{
+		$(".cancel-button").off('click').on('click', $("#dialog-wrapper, .dialog").fadeOut);
+		function checkSyntax() {
+			try {
 				var f = createSandboxedColorFunction($("#colorFunction").val());
 				var testVal = f(0, 0, 0); // not really an in-depth test...
-				if(testVal == undefined || typeof testVal != "string")
-				{
+				if(testVal == undefined || typeof testVal != "string") {
 					$("#checkSyntaxResult").html("<span style='color: red'>The function has to return a string.</span>");
 					return false;
-				}
-				else
-				{
+				} else {
 					$("#checkSyntaxResult").html("<span style='color: green'>Syntax is valid.</span>");
 					return true;
 				}
-			}
-			catch(e)
-			{
+			} catch(e) {
 				$("#checkSyntaxResult").html("<span style='color: red'>" + e + "</span>");
 				return false;
 			}
 		};
 		$("#checkSyntax").off('click').on('click', checkSyntax);
-		$("#dialog-colorFunction .confirm-button").off('click').on('click', function()
-		{
-			if(checkSyntax())
-			{
+		$("#dialog-colorFunction .confirm-button").off('click').on('click', () => {
+			if(checkSyntax()) {
 				$("#dialog-wrapper, .dialog").fadeOut();
 				settings.colorFunctionPreset = "custom";
 				settings.colorFunction = createSandboxedColorFunction($("#colorFunction").val());
 			}
 		});
 	},
-	redrawAll : function()
-	{
+	redrawAll : function() {
 		$("*").addClass("wait");
 		// call is delayed to let the browser show the wait-cursor
-		setTimeout(function()
-		{
-			maze.redrawAll(settings.backgroundColor, settings.colorFactor, settings.colorFunction);
+		setTimeout(function() {
+			maze.redrawAll(settings);
 			$("*").removeClass("wait");
 		}, 100);
 	},
-	startMaze : function()
-	{
-		maze.start(settings.startX, settings.startY, settings.blockSize,
-			settings.cellDistance, settings.directionShuffleProbability,
-			settings.disallowSameDirection, settings.circleProbability,
-			settings.backgroundColor, settings.colorFactor,
-			settings.colorFunction, settings.speed);
+	startMaze : function() {
+		maze.start(settings);
+	},
+	viewSource : function() {
+		window.location.href = "https://github.com/Robyt3/maze";
 	}
 };
 
 var gui = new dat.gui.GUI({ width: 400 });
 
 var folderGeneration = gui.addFolder('Generation');
-folderGeneration.add(settings, 'running').name('Running').onChange(function()
-{
-	maze.setRunning(settings.running);
-});
-folderGeneration.add(settings, 'speed').min(1).max(1000).step(1).name('Speed').onChange(function()
-{
-	maze.setSpeed(settings.speed);
-});
+folderGeneration.add(settings, 'running').name('Running [Space]').listen().onChange(() => maze.setRunning(settings.running));
+folderGeneration.add(settings, 'speed').min(1).max(1000).step(1).name('Speed [+/-]').listen().onChange(() => maze.setSpeed(settings.speed));
+folderGeneration.add(settings, 'performStep').name('Perform single step [S]');
 folderGeneration.add(settings, 'complete').name('Complete generation');
-folderGeneration.add(settings, 'startMaze').name('Restart generation');
+folderGeneration.add(settings, 'startMaze').name('Start/Restart generation [R]');
 folderGeneration.open();
 
 var folderSettings = gui.addFolder('Settings');
@@ -179,14 +142,10 @@ folderColors.addColor(settings, 'backgroundColor').name('Background color');
 folderColors.add(settings, 'colorFactor').min(0).step(1).name('Color factor');
 var colorFuncs = Object.keys(colorFunctions);
 colorFuncs.push("custom");
-folderColors.add(settings, 'colorFunctionPreset', colorFuncs).name('Color function').onChange(function(value)
-{
-	if(value == "custom")
-	{
+folderColors.add(settings, 'colorFunctionPreset', colorFuncs).name('Color function').onChange(value => {
+	if(value == "custom") {
 		settings.changeColorFunction();
-	}
-	else
-	{
+	} else {
 		settings.colorFunction = colorFunctions[value];
 	}
 }).listen();
@@ -194,14 +153,30 @@ folderColors.add(settings, 'changeColorFunction').name('Custom color function');
 folderColors.add(settings, 'redrawAll').name('Redraw with new color');
 folderColors.open();
 
+gui.add(settings, 'viewSource').name('Show source code on GitHub');
+
 $(document).ready(settings.startMaze);
+$(document).keypress(event => {
+	if(event.key == " ") {
+		settings.running = !settings.running;
+		maze.setRunning(settings.running);
+	} else if(event.key == "s") {
+		maze.performStep();
+	} else if(event.key == "r") {
+		settings.startMaze();
+	} else if(event.key == "+") {
+		settings.speed = Math.min(settings.speed + (event.shiftKey ? 10 : 1), 1000);
+		maze.setSpeed(settings.speed);
+	} else if(event.key == "-") {
+		settings.speed = Math.max(settings.speed - (event.shiftKey ? 10 : 1), 1);
+		maze.setSpeed(settings.speed);
+	}
+});
 
 // enable tabs in the color function textarea
 // https://stackoverflow.com/a/6140696
-$("#colorFunction").keydown(function(e)
-{
-	if(e.keyCode === 9) // tab was pressed
-	{
+$("#colorFunction").keydown(function(e) {
+	if(e.keyCode === 9) { // tab was pressed
 		// set textarea value to: text before caret + tab + text after caret
 		$(this).val($(this).val().substring(0, this.selectionStart)
 			+ "\t" + $(this).val().substring(this.selectionEnd));
--- maze.js
+++ maze.js
@@ -1,5 +1,5 @@
-(function(maze, $, undefined)
-{
+(function(maze, $, undefined) {
+
 	// cross-browser support for requestAnimationFrame and cancelAnimationFrame
 	var requestAnimFrame = window.requestAnimationFrame
 		|| window.webkitRequestAnimationFrame
@@ -14,13 +14,11 @@
 		|| window.msCancelRequestAnimationFrame || window.msCancelAnimationFrame
 		|| function(id) { clearTimeout(id); };
 
-	var Direction = function(x, y)
-	{
+	var Direction = function(x, y) {
 		this.x = x;
 		this.y = y;
 	}
-	Direction.prototype.isOpposite = function(other)
-	{
+	Direction.prototype.isOpposite = function(other) {
 		return (this.x != 0 && this.x == -other.x)
 			|| (this.y != 0 && this.y == -other.y);
 	}
@@ -30,36 +28,28 @@
 	Direction.UP 	= new Direction(0, -1);
 	Direction.DOWN 	= new Direction(0, 1);
 
-	var Cell = function(x, y, color, fromDir)
-	{
+	var Cell = function(x, y, color, fromDir) {
 		this.x = x;
 		this.y = y;
 		this.color = color;
 		this.fromDir = fromDir;
 	}
 
-	var Settings = function(startX, startY, blockSize, cellDistance, directionShuffleProbability,
-		disallowSameDirection, circleProbability, backgroundColor, colorFactor, colorFunction)
-	{
-		this.startX = startX;
-		this.startY = startY;
-		this.blockSize = blockSize;
-		this.cellDistance = cellDistance;
-		this.directionShuffleProbability = directionShuffleProbability;
-		this.disallowSameDirection = disallowSameDirection;
-		this.circleProbability = circleProbability;
-		this.backgroundColor = backgroundColor;
-		this.colorFactor = colorFactor;
-		this.colorFunction = colorFunction;
+	var Settings = function(guiSettings) {
+		this.startX = guiSettings.startX;
+		this.startY = guiSettings.startY;
+		this.blockSize = Math.floor(guiSettings.blockSize);
+		this.cellDistance = Math.floor(guiSettings.cellDistance);
+		this.directionShuffleProbability = guiSettings.directionShuffleProbability;
+		this.disallowSameDirection = guiSettings.disallowSameDirection;
+		this.circleProbability = guiSettings.circleProbability;
+		this.backgroundColor = guiSettings.backgroundColor;
+		this.colorFactor = Math.floor(guiSettings.colorFactor);
+		this.colorFunction = guiSettings.colorFunction;
 	}
 	Settings.prototype = {
-		get border()
-		{
-			if(this.cellDistance == 1)
-			{
-				return 0;
-			}
-			return 1;
+		get border() {
+			return this.cellDistance == 1 ? 0 : 1;
 		}
 	}
 
@@ -80,18 +70,16 @@
 	var context;
 	var bufferCanvas;
 	var bufferContext;
-	var changedCells;
+	var changedCells = new Array();
 
 	/**
 	 * Shuffles array in place.
 	 * @see http://stackoverflow.com/a/6274381
 	 * @param {Array} a items The array containing the items.
 	 */
-	function shuffle(a)
-	{
+	function shuffle(a) {
 		var j, x, i;
-		for(i = a.length; i; i--)
-		{
+		for(i = a.length; i; i--) {
 			j = Math.floor(Math.random() * i);
 			x = a[i - 1];
 			a[i - 1] = a[j];
@@ -99,44 +87,41 @@
 		}
 	}
 
-	function initCanvas()
-	{
-		// get canvas
+	function initCanvas() {
 		canvas = document.getElementById("canvas");
-		canvas.mozOpaque = true;
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
-		context = canvas.getContext("2d");
-
-		// buffer canvas
 		bufferCanvas = document.createElement("canvas");
-		bufferCanvas.width = canvas.width;
-		bufferCanvas.height = canvas.height;
+
+		[canvas, bufferCanvas].forEach(can => {
+			can.mozOpaque = true;
+			can.width = window.innerWidth;
+			can.height = window.innerHeight;
+		});
+
+		context = canvas.getContext("2d");
 		bufferContext = bufferCanvas.getContext("2d");
 
-		changedCells = new Array();
+		[context, bufferContext].forEach(ctx => {
+			ctx.webkitImageSmoothingEnabled = false;
+			ctx.mozImageSmoothingEnabled = false;
+			ctx.imageSmoothingEnabled = false;
+		});
 	}
 
-	function initMaze()
-	{
-		// maze size
+	function initMaze() {
 		width = Math.floor(canvas.width / settings.blockSize);
 		height = Math.floor(canvas.height / settings.blockSize);
 
-		// initialize maze array
 		data = new Array(height);
-		for(var y = 0; y < height; y++)
-		{
+		changedCells.length = 0;
+		for(var y = 0; y < height; y++) {
 			data[y] = new Array(width);
-			for(var x = 0; x < width; x++)
-			{
+			for(var x = 0; x < width; x++) {
 				data[y][x] = null;
 			}
 		}
 	}
 
-	function initGeneration()
-	{
+	function initGeneration() {
 		// put starting position on the stack
 		dfsStack = new Array();	
 		var border = settings.border;
@@ -146,28 +131,21 @@
 		directions = [ Direction.RIGHT, Direction.LEFT, Direction.UP, Direction.DOWN ];
 	}
 
-	function updateCell()
-	{
+	maze.performStep = function() {
 		var cell;
 		var makeCircle = false;
 
 		// get next cell
-		while(true)
-		{
-			if(dfsStack.length == 0)
-			{
+		while(true) {
+			if(dfsStack.length == 0) {
 				return false;
 			}
 
 			cell = dfsStack.pop();
-			if(data[cell.y][cell.x] != null)
-			{
-				if(Math.random() < settings.circleProbability && settings.cellDistance > 1)
-				{
+			if(data[cell.y][cell.x] != null) {
+				if(Math.random() < settings.circleProbability && settings.cellDistance > 1) {
 					makeCircle = true;
-				}
-				else
-				{
+				} else {
 					continue;
 				}
 			}
@@ -179,10 +157,8 @@
 		var color = cell.color;
 
 		// draw the intermediate cells
-		if(cell.fromDir !== Direction.NONE)
-		{
-			for(var i = 1; i < settings.cellDistance; i++)
-			{
+		if(cell.fromDir !== Direction.NONE) {
+			for(var i = 1; i < settings.cellDistance; i++) {
 				// minus fromDir, since the directions points from old to new position (p is the new position)
 				var intermediate = new Cell(cell.x - i * cell.fromDir.x, cell.y - i * cell.fromDir.y, color);
 				data[intermediate.y][intermediate.x] = intermediate;
@@ -205,8 +181,7 @@
 
 		// expand maze in all directions
 		// opposite order so that the first element will be the top-most on the stack
-		for(var i = directions.length-1; i >= 0; i--)
-		{
+		for(var i = directions.length-1; i >= 0; i--) {
 			var d = directions[i];
 
 			// no need to check the direction that we are coming from
@@ -232,118 +207,92 @@
 		return true;
 	}
 
-	function redrawChanged()
-	{
-		while(changedCells.length > 0)
-		{
-			drawCell(changedCells.pop());
-		}
+	function redrawChanged() {
+		changedCells.forEach(drawCell);
+		changedCells.length = 0;
 	}
 
-	maze.redrawAll = function(backgroundColor, colorFactor, colorFunction)
-	{
-		settings.backgroundColor = backgroundColor;
-		settings.colorFactor = colorFactor;
-		settings.colorFunction = colorFunction;
+	maze.redrawAll = function(guiSettings) {
+		settings.backgroundColor = guiSettings.backgroundColor;
+		settings.colorFactor = guiSettings.colorFactor;
+		settings.colorFunction = guiSettings.colorFunction;
 
-		for(var y = 0; y < height; y++)
-		{
-			for(var x = 0; x < width; x++)
-			{
-				if(data[y][x] != null)
-				{
+		for(var y = 0; y < height; y++) {
+			for(var x = 0; x < width; x++) {
+				if(data[y][x] != null) {
 					drawCell(data[y][x]);
 				}
 			}
 		}
 	}
 
-	function drawCell(cell)
-	{
-		if(settings.colorFactor == 0)
-		{
+	function drawCell(cell) {
+		if(settings.colorFactor == 0) {
 			bufferContext.fillStyle = "#ffffff";
-		}
-		else
-		{
+		} else {
 			bufferContext.fillStyle = settings.colorFunction(
 				(cell.color % settings.colorFactor) / settings.colorFactor,
-				cell.x / width, cell.y / height);
+				cell.x / width,
+				cell.y / height);
 		}
 
-		bufferContext.fillRect(cell.x * settings.blockSize, cell.y * settings.blockSize,
-			settings.blockSize, settings.blockSize);
+		bufferContext.fillRect(
+			cell.x * settings.blockSize,
+			cell.y * settings.blockSize,
+			settings.blockSize,
+			settings.blockSize);
 	}
 
-	function drawBuffer()
-	{
+	function drawBuffer() {
 		context.fillStyle = settings.backgroundColor;
 		context.fillRect(0, 0, canvas.width, canvas.height);
 		context.drawImage(bufferCanvas, 0, 0);
 	}
 
-	function updateAndDrawFrame()
-	{
-		if(running)
-		{
-			for(var i = 0; i < updatesPerFrame; i++)
-			{
-				if(!updateCell())
-				{
+	function updateAndDrawFrame() {
+		if(running) {
+			for(var i = 0; i < updatesPerFrame; i++) {
+				if(!maze.performStep()) {
 					break;
 				}
 			}
-			redrawChanged();
 		}
+		redrawChanged();
 		drawBuffer();
 		animFrameReqId = requestAnimFrame(updateAndDrawFrame);
 	};
 
-	maze.start = function(startX, startY, blockSize, cellDistance, directionShuffleProbability,
-		disallowSameDirection, circleProbability, backgroundColor, colorFactor, colorFunction, speed)
-	{
-		settings = new Settings(startX, startY, blockSize, cellDistance, directionShuffleProbability,
-			disallowSameDirection, circleProbability, backgroundColor, colorFactor, colorFunction);
-		updatesPerFrame = speed;
-		running = true;
+	maze.start = function(guiSettings) {
+		settings = new Settings(guiSettings);
+		maze.setSpeed(guiSettings.speed);
+		maze.setRunning(guiSettings.running);
 
 		initCanvas();
 		initMaze();
 		initGeneration();
 
 		// start the draw-loop
-		if(animFrameReqId != null)
-		{
+		if(animFrameReqId != null) {
 			cancelAnimFrame(animFrameReqId);
 		}
 		animFrameReqId = requestAnimFrame(updateAndDrawFrame);
 	}
 
-	maze.setSpeed = function(_updatesPerFrame)
-	{
-		if(_updatesPerFrame < 1)
-			updatesPerFrame = 1;
-		else if(_updatesPerFrame > 10000)
-			updatesPerFrame = 10000;
-		else
-			updatesPerFrame = _updatesPerFrame;
+	maze.setSpeed = function(_updatesPerFrame) {
+		updatesPerFrame = Math.min(Math.max(_updatesPerFrame, 1), 10000);
 	}
 
-	maze.completeGeneration = function()
-	{
-		while(updateCell())
-		{
-			// updateCell will return false when no more cells are left
+	maze.completeGeneration = function() {
+		while(maze.performStep()) {
+			// performStep will return false when no more cells are left
 		}
 	}
 
-	maze.setRunning = function(_running)
-	{
+	maze.setRunning = function(_running) {
 		running = _running;
 	}
 
-	maze.isRunning = function()
-	{
+	maze.isRunning = function() {
 		return running;
 	}
 }(window.maze = window.maze || {}, jQuery));
